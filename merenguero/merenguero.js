e13898d273262306ff335bbad51d1349123837e1
Santiago Chávez <sanxofon@users.noreply.github.com>
2023-02-26 23:03:47 -0600

    Merenguero

--- merenguero/merenguero.js
+++ merenguero/merenguero.js
@@ -1,36 +1,49 @@
 let heads = 0;
 let tails = 0;
+let venta = 0;
+let apuesta = 0;
+let segundos = 1;
+let infinito=false;
 let coin = document.querySelector(".coin");
 let flipBtn = document.querySelector("#flip-button");
 let resetBtn = document.querySelector("#reset-button");
 
-flipBtn.addEventListener("click", () => {
+flipBtn.addEventListener("click", bolado);
+function bolado() {
     let i = Math.floor(Math.random() * 2);
     coin.style.animation = "none";
     if(i){
+        // Águila: Cliente gana dos merengues
         setTimeout(function(){
-            coin.style.animation = "spin-heads 2s forwards";
+            coin.style.animation = "spin-heads "+segundos+"s forwards";
         }, 100);
         heads++;
+        apuesta++;
     }
     else{
+        // Sol: Cliente no lleva merengue
         setTimeout(function(){
-            coin.style.animation = "spin-tails 2s forwards";
+            coin.style.animation = "spin-tails "+segundos+"s forwards";
         }, 100);
         tails++;
     }
-    setTimeout(updateStats, 2000);
+    venta++;
+    setTimeout(updateStats, segundos*1000-200);
     disableButton();
-});
+}
 function updateStats(){
-    document.querySelector("#heads-count").textContent = `Águila: ${heads} (`+(Math.round(1000*(heads)/(tails+heads))/10)+"%)";
-    document.querySelector("#tails-count").textContent = `Sol: ${tails} (`+(Math.round(1000*(tails)/(tails+heads))/10)+"%)";
+    document.querySelector("#heads-count").innerHTML = `Águila: ${heads}<br>(`+(Math.round(1000*(heads)/(tails+heads))/10)+"%)";
+    document.querySelector("#tails-count").innerHTML = `Sol: ${tails}<br>(`+(Math.round(1000*(tails)/(tails+heads))/10)+"%)";
+    document.querySelector("#apuesta").innerHTML = '<img src="merengue.png" width="60"><br>'+(apuesta*2);
+    document.querySelector("#venta").innerHTML = '<img src="dinero.png" width="60"><br>'+venta;
+    document.querySelector("#indice").innerHTML = ((Math.round(10000*(venta)/(venta+apuesta*2))-5000)/100)+"%"
 }
 function disableButton(){
     flipBtn.disabled = true;
     setTimeout(function(){
         flipBtn.disabled = false;
-    },3000);
+        if(infinito)flipBtn.click();
+    },segundos*1000);
 }
 resetBtn.addEventListener("click",() => {
     coin.style.animation = "none";
@@ -38,8 +51,8 @@ resetBtn.addEventListener("click",() => {
     tails = 0;
     updateStats();
 });
-
-// Tira bolados infinitamente
-/* setInterval(function(){
-  flipBtn.click()
-},5000); */
+document.querySelector("#infinito").addEventListener("change",(ev) => {
+    infinito = document.querySelector("#infinito").checked;
+    if(infinito)flipBtn.innerHTML='<big>&infin;</big> Bolados';
+    else flipBtn.innerHTML='1 Bolado';
+});
